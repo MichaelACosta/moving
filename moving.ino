073d178b2311019c67e033bba03704b38120c815
Michael Costa <macosta@inf.ufpel.edu.br>
2018-09-05 22:06:10 -0300

    moving: added a function of movement and control
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- moving.ino
+++ moving.ino
@@ -1,5 +1,6 @@
 const int pwmY = 5;
 const int pwmX = 6;
+char directions = 'p';
 char control;
 
 void setup() {
@@ -10,28 +11,53 @@ void setup() {
   Serial.begin(9600);
 }
 
+void setDirections() {
+    if (control == 'd'){
+      directions = 'd';
+    }
+    else if (control == 'a'){
+      directions = 'a';
+    }
+    else if (control == 'w'){
+      directions = 'w';
+    }
+    else if (control == 's'){
+      directions = 's';
+    }
+    else if (control == 'p'){
+      directions = 'p';
+    }
+}
+
+void moving(){
+  switch (directions) {
+    case 'd':
+      analogWrite(pwmX, 80);
+      break;
+    case 'a':
+      analogWrite(pwmX, 190);
+      break;
+    case 'w':
+      analogWrite(pwmY, 190);
+      break;
+    case 's':
+      analogWrite(pwmY, 80);
+      break;
+    case 'p':
+      analogWrite(pwmY, 135);
+      analogWrite(pwmX, 135);
+      break;
+    default:
+      analogWrite(pwmY, 135);
+      analogWrite(pwmX, 135);
+      break;
+  }
+}
+
 void loop() {
   if (Serial.available() > 0) {
     control = Serial.read();
-
-    switch (control) {
-      case 'd':
-        analogWrite(pwmX, 80);
-        break;
-      case 'a':
-        analogWrite(pwmX, 190);
-        break;
-      case 'w':
-        analogWrite(pwmY, 190);
-        break;
-      case 's':
-        analogWrite(pwmY, 80);
-        break;
-      default:
-        analogWrite(pwmY, 135);
-        analogWrite(pwmX, 135);
-        break;
-    }
-    delay(1000);
   }
+  setDirections();
+  moving();
 }
