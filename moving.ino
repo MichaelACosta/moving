7a6277420d2e0be3ac5f17f20f10f5a0a5cbe33d
William Kolodziejski <william.kol.wk@gmail.com>
2018-10-24 13:56:08 -0300

    Code refactored and left wheel light sensor code attached

--- moving.ino
+++ moving.ino
@@ -1,63 +1,58 @@
-const int pwmY = 5;
-const int pwmX = 6;
-char directions = 'p';
-char control;
+#define pwmY 5
+#define pwmX 6
+#define light_port 8
+
+bool light_state = false;
+unsigned int left_counter = 0; 
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(pwmY, OUTPUT);
   pinMode(pwmX, OUTPUT);
   analogWrite(pwmY, 135);
   analogWrite(pwmX, 135);
-  Serial.begin(9600);
-}
-
-void setDirections() {
-    if (control == 'd'){
-      directions = 'd';
-    }
-    else if (control == 'a'){
-      directions = 'a';
-    }
-    else if (control == 'w'){
-      directions = 'w';
-    }
-    else if (control == 's'){
-      directions = 's';
-    }
-    else if (control == 'p'){
-      directions = 'p';
-    }
-}
-
-void moving(){
-  switch (directions) {
-    case 'd':
-      analogWrite(pwmX, 80);
-      break;
-    case 'a':
-      analogWrite(pwmX, 190);
-      break;
-    case 'w':
-      analogWrite(pwmY, 190);
-      break;
-    case 's':
-      analogWrite(pwmY, 80);
-      break;
-    case 'p':
-      analogWrite(pwmY, 135);
-      analogWrite(pwmX, 135);
-      break;
-    default:
-      analogWrite(pwmY, 135);
-      analogWrite(pwmX, 135);
-      break;
-  }
+  
+  pinMode(light_port, INPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    control = Serial.read();
+    _move(Serial.read());
   }
-  setDirections();
-  moving();
+  
+  int light_sensor = digitalRead(light_port);
+
+  if (light_sensor == 1 && !light_state) {
+      light_state = true;
+      Serial.println(left_counter++);
+    } else if (light_sensor == 0 && light_state) {
+      light_state = false;
+    }
+}
+
+void _move(char control){
+  switch (control) {
+    case 'd':
+      analogWrite(pwmX, 80);
+      Serial.println("Right");
+      break;
+    case 'a':
+      analogWrite(pwmX, 190);
+      Serial.println("Left");
+      break;
+    case 'w':
+      analogWrite(pwmY, 190);
+      analogWrite(pwmX, 145);
+      Serial.println("Forward");
+      break;
+    case 's':
+      analogWrite(pwmY, 80);
+      Serial.println("Backward");
+      break;
+     case 'p':
+      analogWrite(pwmY, 135);
+      analogWrite(pwmX, 135);
+      break;
+  }
 }
